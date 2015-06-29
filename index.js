b5f6eb1e4f1f8039610c13e9cf184d8562fa7985
Pai Peng <sipaipv6@gmail.com>
2015-06-29 22:26:41 +0200

    update

--- index.js
+++ index.js
@@ -1,75 +1,62 @@
 'use strict';
 var jsons = require('./fonts.json');
-/*
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
-};
-*/
 
 function isChineseSimple(html) {
-    for (var i = 0; i < jsons.length; i++) {
-        var json = jsons[i];
+  for (var i = 0; i < jsons.length; i++) {
+    var json = jsons[i];
 
-        if (html.indexOf(json.jf) > -1) {
-          //console.log("found " + json.jf);
-            return true;
-        }
+    if (html.indexOf(json.jf) > -1) {
+      //console.log("found " + json.jf);
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function doTranslation(html, mode) {
-    var search = undefined;
-    var replace = undefined;
+  var search;
+  var replace;
 
-    var isSimple = true;
-    if (mode === undefined) {
-      isSimple = isChineseSimple(html);
+  var isSimple = true;
+  if (mode === undefined) {
+    isSimple = isChineseSimple(html);
 
-    } else if (mode === 's2t') {
-      isSimple = true;
-    } else if (mode === 't2s') {
-      isSimple = false;
+  } else if (mode === 's2t') {
+    isSimple = true;
+  } else if (mode === 't2s') {
+    isSimple = false;
+  }
+
+  for (var i = 0; i < jsons.length; i++) {
+    var json = jsons[i];
+    if (isSimple) {
+      search = json.jf;
+      replace = json.fj;
+    } else {
+      search = json.fj;
+      replace = json.jf;
     }
-
-    for (var i = 0; i < jsons.length; i++) {
-        var json = jsons[i];
-        if (isSimple) {
-            search = json.jf;
-            replace = json.fj;
-        } else {
-            search = json.fj;
-            replace = json.jf;
-        }
-        html = html.replace(new RegExp(search, 'g'), replace);
-    }
-    return html;
+    html = html.replace(new RegExp(search, 'g'), replace);
+  }
+  return html;
 }
-
 
 exports.translate = function(text, mode) {
-  //console.log("translator " + text);
-
   return doTranslation(text, mode);
-}
+};
 
 exports.translateFile = function(srcFile, dstFile, mode, callback) {
   var fs = require('fs');
-  fs.readFile(srcFile, 'utf8', function (err,data) {
-  if (err) {
-    callback(err);
-  } else {
-    //console.log(data);
-    var ret = doTranslation(data, mode);
-    var dst = (dstFile !== undefined)?dstFile:srcFile;
-    fs.writeFile(dst, ret, function(err) {
-        if(err) {
-            callback(err);
-        }
-
-        //console.log("The file was saved!");
+  fs.readFile(srcFile, 'utf8', function(err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      //console.log(data);
+      var ret = doTranslation(data, mode);
+      var dst = (dstFile !== undefined) ? dstFile : srcFile;
+      fs.writeFile(dst, ret, function(err) {
         callback(err);
-    });
-  }
-});
-}
+      });
+    }
+  });
+};
--- test/test.js
+++ test/test.js
@@ -2,9 +2,53 @@
 var assert = require('assert');
 var chineseSimpleTraditionTranslator = require('../');
 
-describe('chinese-simple-tradition-translator node module', function () {
-  it('must have at least one test', function () {
-    chineseSimpleTraditionTranslator();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+describe('chinese-simple-tradition-translator node module', function() {
+  it('test translate from simple to traditional', function() {
+    var ret = chineseSimpleTraditionTranslator.translate("三国演义");
+    assert.equal(ret, '三國演義');
+  });
+
+  it('test translate from traditional to simple', function() {
+    var ret = chineseSimpleTraditionTranslator.translate("三國演義");
+    assert.equal(ret, '三国演义');
+  });
+  it('test translate from simple to traditional with mode', function() {
+    var ret = chineseSimpleTraditionTranslator.translate("三国演义", "s2t");
+    assert.equal(ret, '三國演義');
+  });
+
+  it('test translate from simple to traditional with mode', function() {
+    var ret = chineseSimpleTraditionTranslator.translate("三国演义", "t2s");
+    assert.equal(ret, '三国演义');
+  });
+
+
+  it('test translate from traditional to simple with mode', function() {
+    var ret = chineseSimpleTraditionTranslator.translate("三國演義", "t2s");
+    assert.equal(ret, '三国演义');
+  });
+
+
+  it('test translate from traditional to simple with mode', function() {
+    var ret = chineseSimpleTraditionTranslator.translate("三國演義", "s2t");
+    assert.equal(ret, '三國演義');
+  });
+
+
+  it('test translate file from simple to traditional with mode', function() {
+    chineseSimpleTraditionTranslator.translateFile("test_s.json", "output.json", undefined, function(err) {
+      assert(false, err);
+      var json = require("output2.json");
+      assert.equal(json.text, false);
+    });
+  });
+
+  it('test translate file from traditional to simple with mode', function() {
+    chineseSimpleTraditionTranslator.translateFile("test_t.json", "output.json", "t2s", function(err) {
+      assert(false, err);
+      var json = require("output.json");
+      console.log("json text " + json.text);
+      assert.equal(json.text, "三國演義");
+    });
   });
 });
